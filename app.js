3c35fb752afd0b7ec16aec562ce5c5a8c7911200
John Mabiala <johnmabiala@hotmail.com>
2025-10-22 09:11:36 +0100

    Added bootstrap and css

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 import express from "express";
+import path from "node:path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const publicPath = path.join(__dirname, "public");
+
+import { requireLogin } from "./middleware.js";
+import loginRoute from "./routes/loginRoutes.js";
 
 const app = express();
 const port = 3003;
@@ -9,8 +18,12 @@ const server = app.listen(port, () =>
 
 app.set("view engine", "pug");
 app.set("views", "views");
+app.use(express.static(publicPath));
 
-app.get("/", (req, res, next) => {
+// Routes
+app.use("/login", loginRoute);
+
+app.get("/", requireLogin, (req, res, next) => {
   let payload = {
     pageTitle: "Home",
   };
